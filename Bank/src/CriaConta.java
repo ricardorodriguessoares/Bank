afa3ace54052945e89b3d8916f6e1d4a0618708d
Ricardo Rodrigues Soares <55320016+ricardorodriguessoares@users.noreply.github.com>
2021-01-16 09:16:24 -0300

    Linhas Adicionadas / Added lines
    Adicionei novas linhas ao código, incluído referência a o novo objeto e o if para confirmar as contas en questão
    I added new lines to the code, including reference to the new object and the if to confirm the accounts in question

--- Bank/src/CriaConta.java
+++ Bank/src/CriaConta.java
@@ -1,4 +1,6 @@
-
+// Projeto inicial da criação de um Banco /Initial project to create a Bank
+//Autour: Ricardo Rodrigues
+//email: ri.rodriguesoares@gmail.com  - phone +55 11 96160 7406
 public class CriaConta {
 
 		public static void main(String [] args) {
@@ -14,6 +16,24 @@ public class CriaConta {
 			
 			System.out.println("A primeira conta tem " + primeiraConta.saldo);
 			System.out.println("A segunda conta tem " + segundaConta.saldo);
+			
+			System.out.println(primeiraConta.agencia);
+			System.out.println(primeiraConta.numero);
+			
+			//Verificando se as contas são as mesmas
+			//Verifying that the accounts are the same
+			if(primeiraConta == segundaConta) {
+				System.out.println("São a Mesma conta");
+			}else {
+				System.out.println("Contas Diferentes");
+			}
+			
+			//Confirmando que são duas referências porém para objeto diferente
+			// Confirming that they are two references but for a different object
+			//imprimindo o local de armazenamento na memória
+			// printing the storage location in memory
+			System.out.println(primeiraConta);
+			System.out.println(segundaConta);
 			
 		}
 }
